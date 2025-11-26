2875c704423501d8501785762d541947de8d8997
fadelthiaw7-beep <fadelthiaw7@gmail.com>
2025-11-26 16:14:51 +0100

    css page connexion et inscription (#17)
    Co-authored-by: mathisklein33 <mathisklein33@gmail.com>

--- page/produit-creation.php
+++ page/produit-creation.php
@@ -47,10 +47,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Date actuelle
     $date = date('Y-m-d');
 
-    /* ---------------------------------------------
-       Insertion dans la base de donnée
-       (image = nom du fichier uniquement)
-    --------------------------------------------- */
+
+       //Insertion dans la base de donnée
+
     $stmt = $conn->prepare("
         INSERT INTO produit (nom, description, prix, stock, image, date, user_iduser, user_role_idrole)
         VALUES (?, ?, ?, ?, ?, ?, ?, ?)

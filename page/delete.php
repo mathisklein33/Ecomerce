ace9abcab2553b2b1511cae62a8acb9c77d1e983
mathis klein <mathisklein33@gmail.com>
2025-11-27 15:19:43 +0100

    admin produis délecte et modification

---
+++ page/delete.php
@@ -0,0 +1,26 @@
+﻿<?php
+require '../public/config/config.php';
+
+// Vérification de l'ID
+if (!isset($_GET['idproduit']) || !is_numeric($_GET['idproduit'])) {
+    die("ID invalide");
+}
+
+$id = (int) $_GET['idproduit'];
+
+// Vérifier la connexion
+if ($conn->connect_error) {
+    die('Erreur : ' . $conn->connect_error);
+}
+
+// Requête SQL DELETE
+$sql = "DELETE FROM produit WHERE idproduit = $id";
+
+if (mysqli_query($conn, $sql)) {
+    mysqli_close($conn);
+    header('Location: admin-produit.php');
+    exit;
+} else {
+    echo "Erreur delete : " . mysqli_error($conn);
+}
+?>

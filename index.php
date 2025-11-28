2df492b0d910c33b069e8ae4c56cefc598b932ac
mathis klein <mathisklein33@gmail.com>
2025-11-28 15:11:11 +0100

    gestion lien et rooter

--- index.php
+++ index.php
@@ -15,6 +15,8 @@ $routes = [
         'produit/modifier' => __DIR__ . '/page/modifier.php',
         'produit/supprimer' => __DIR__ . '/page/delete.php',
         'user' => __DIR__ . '/page/user.php',
+        'panier'=> __DIR__ . '/page/panier.php',
+        'cathalog'=> __DIR__ . '/page/catalogue.php',
 ];
 
 $page = $_GET['page'] ?? 'base';
--- public/includes/header.php
+++ public/includes/header.php
@@ -20,21 +20,21 @@
                         <a class="nav-link" href="index.php">Accueil</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Catalogue</a>
+                        <a class="nav-link" href="http://localhost/savouinos/?page=cathalog">Catalogue</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="http://localhost/savouinos/?page=login">Connection</a>
+                        <a class="nav-link" href="#">Contact</a>
                     </li>
                 </ul>
             </nav>
         <div class="d-flex align-items-center gap-5">
     <div class="header-icon">
-        <a href="#"><svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
+        <a href="http://localhost/savouinos/?page=login"><svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
                 <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z"/>
             </svg></a>
     </div>
     <div class="header-icon">
-        <a href="#"><svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
+        <a href="http://localhost/savouinos/?page=panier"><svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
                 <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l1.313 7h8.17l1.313-7zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2"/>
             </svg></a>
     </div>

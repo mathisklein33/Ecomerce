26f5c504e691b7ccaa3697dbd02f8679b5e4dc8f
mathisklein33 <mathisklein33@gmail.com>
2025-12-01 13:59:54 +0100

    page 404 debug (#40)

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 ﻿<?php
-include 'public/includes/header.php';
 include 'public/config/config.php';
+$displayHeader = true;
+$displayFooter = true;
+if($displayHeader) include 'public/includes/header.php';
 
 
 $routes = [
@@ -25,6 +27,6 @@ $view = $routes[$page] ?? __DIR__ . '/page/404.php';
 
 include_once $view;
 
-include 'public/includes/footer.php';
+if($displayFooter) include 'public/includes/footer.php';
 ?>
 
